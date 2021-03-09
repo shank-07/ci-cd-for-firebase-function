acaa7412acd3ed2752e8765401753823dcc97d30
shank-07 <shank.07manve@gmail.com>
2021-03-09 16:34:14 +0530

    testing for two request

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,35 @@ const functions = require("firebase-functions");
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.dummyfunction = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+exports.dummyfunction1 = functions.https.onRequest((request, response) => {
+  functions.logger.info("Dummy Function 1", {structuredData: true});
+
+  const {data1, data2 } = request.body;
+
+  console.log(data1);
+  console.log(data2);
+
   response.status(200).json({
       status:  "SUCCESS",
-      message : "dummy fuintion for ci cd"
+      message : "this is cloud function 1",
+      msg: "req_2"
+
+  });
+  return;
+});
+
+exports.dummyfunction2 = functions.https.onRequest((request, response) => {
+  functions.logger.info("Dummy Function 2", {structuredData: true});
+
+  const {data1, data2 } = request.body;
+
+  console.log(data1);
+  console.log(data2);
+
+  response.status(200).json({
+      status:  "SUCCESS",
+      message : "this is cloud function 2",
+      msg: "req_2"
   });
   return;
 });
